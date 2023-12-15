0af78d7644d586d6f2285858a71c7cf0f50e4fbf
EnesHHS <21115745@student.hhs.nl>
2023-12-15 12:13:54 +0100

    created login view and required components to properly display the view

--- stichtingaccessibility.client/src/App.jsx
+++ stichtingaccessibility.client/src/App.jsx
@@ -1,8 +1,44 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-    return<div><h3>Welcome</h3></div>
-}
+function MyButton() {
+    return (
+      <button>I'm a button</button>
+    );
+  }
 
-export default App;
+  function MyForm() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log('Email:', email);
+      console.log('Password:', password);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Email:
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </label>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      );
+    }
+
+  export default function App() {
+    return (
+        <div>
+        <h1>Welcome to my app</h1>
+        <MyForm />
+      </div>
+    );
+  }
