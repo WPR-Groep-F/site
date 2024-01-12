d92bab40f7a089b37b297a4c544710de5e3ba31d
EnesHHS <21115745@student.hhs.nl>
2024-01-12 19:32:49 +0100

    defined queryclient on query page

--- stichtingaccessibility.client/src/pages/QueryTesting/QueryTesting.jsx
+++ stichtingaccessibility.client/src/pages/QueryTesting/QueryTesting.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-import { useQuery } from "react-query";
+import { useQuery, QueryClient, QueryClientProvider } from "react-query";
 import axios from 'axios';
+
+const queryClient = new QueryClient();
 
 const fetchCustomers = async () => {
     const result = await axios.get('http://localhost:5088/api/customer');
     return <result className="data"></result>
 };
 
-const QueryTesting = () => {
+const Customer  = () => {
     const { isLoading, error, data } = useQuery('customers', fetchCustomers); 
     if (isLoading) return "Loading...";
-    if (error) return "There was an error! ${error}";
-
+    if (error) return 'There was an error! ${error}';
 
     return (
         <div>
-        <h1>Query GET</h1>        
+        <h1>Query GET</h1>    
             <div>
                 { data.map((customer) => {
                     <p key={customer} > {customer.FirstName} </p>
@@ -23,10 +24,15 @@ const QueryTesting = () => {
                 }
                 </div>
         </div>
-        
     )
 };
 
-
-export default QueryTesting;
+const QueryTesting = () => {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <Customer />
+      </QueryClientProvider>
+    );
+  };
   
+  export default QueryTesting;
