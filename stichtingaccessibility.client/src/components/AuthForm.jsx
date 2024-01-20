5740307c2372dc2c89b33a413196b1d1f6a7970b
juustins <justinboekhouwer@gmail.com>
2024-01-20 20:09:42 +0100

    added logout feature. added validation errors on loginpage and register page. edited csproj to launch in production if env variable is set to production or to launch in dev if not set or not set to production

--- stichtingaccessibility.client/src/components/AuthForm.jsx
+++ stichtingaccessibility.client/src/components/AuthForm.jsx
@@ -1,4 +1,5 @@
 import classes from "./AuthForm.module.css"
+import rmstyle from "./removestyle.module.css";
 import {Form,useActionData, Link} from "react-router-dom";
 import logo from "../assets/Logo/logo_darkblue.png";
 
@@ -8,43 +9,43 @@ function AuthForm(){
         <>
             <Form method="post">
                 {data && data.errors && (
-                    <ul>
-                        {Object.values(data.errors).map((err) => (
-                            <li key={err}>{err}</li>
+                    <ul className={rmstyle.ulError}>
+                        {Object.entries(data.errors).map(([field, errors]) => (
+                            <li key={field}>{field}: {errors[0]}</li>
                         ))}
                     </ul>
                 )}
                 {data && data.message && <p>{data.message}</p>}
-            <div className={classes["logo"]}>
-              <img src={logo} alt="logo" />
-              <h1>Accessibility</h1>
-            </div>
+                <div className={classes["logo"]}>
+                    <img src={logo} alt="logo" />
+                    <h1>Accessibility</h1>
+                </div>
 
-            <div className={classes["input-box"]}>
-              <input type="username" name="username" placeholder="Username" />
-              <i class="bx bxs-user"></i>
-            </div>
-            <div className={classes["input-box"]}>
-              <input type="Password" name="password" placeholder="Password" />
-              <i class="bx bxs-lock-alt"></i>
-            </div>
-            <button type="Submit" className={classes["btn--login"]}>
-              Login
-            </button>
-            <div className={classes["forgot-password"]}>
-            <Link to={"/forgot"}>Forgot password?</Link>
-              <br/>
-              <Link to={"/register"}>Register here <i class='bx bx-right-arrow-alt' ></i></Link>
-            </div>
-            <div className={classes["OR"]}>
-              <p>OR</p>
-            </div>
-            <div className={classes["Google-btn"]}>
-              <button type="Submit" className={classes["btn--google"]}>
-                Login with google
-              </button>
-              <i class="bx bxl-google"></i>
-            </div>
+                <div className={classes["input-box"]}>
+                    <input type="username" name="username" placeholder="Username" />
+                    <i className="bx bxs-user"></i>
+                </div>
+                <div className={classes["input-box"]}>
+                    <input type="Password" name="password" placeholder="Password" />
+                    <i className="bx bxs-lock-alt"></i>
+                </div>
+                <button type="Submit" className={classes["btn--login"]}>
+                    Login
+                </button>
+                <div className={classes["forgot-password"]}>
+                    <Link to={"/forgot"}>Forgot password?</Link>
+                    <br/>
+                    <Link to={"/register"}>Register here <i className='bx bx-right-arrow-alt' ></i></Link>
+                </div>
+                <div className={classes["OR"]}>
+                    <p>OR</p>
+                </div>
+                <div className={classes["Google-btn"]}>
+                    <button type="Submit" className={classes["btn--google"]}>
+                        Login with google
+                    </button>
+                    <i className="bx bxl-google"></i>
+                </div>
 
             </Form>
         </>
