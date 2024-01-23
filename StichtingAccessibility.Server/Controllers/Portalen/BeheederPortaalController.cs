7ecaca44df7b42128b9aba748a6e5716b8f02a7f
juustins <justinboekhouwer@gmail.com>
2024-01-23 04:32:06 +0100

    Added implemnation for the invite post method to return guid and save invite in db

--- StichtingAccessibility.Server/Controllers/Portalen/BeheederPortaalController.cs
+++ StichtingAccessibility.Server/Controllers/Portalen/BeheederPortaalController.cs
@@ -1,11 +1,22 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using StichtingAccessibility.Server.Models;
 
 namespace StichtingAccessibility.Server.Controllers;
-
+[Authorize(Roles = "Beheerder")]
 [Route("api/[controller]")]
 [ApiController]
 public class BeheerderPortaalController : ControllerBase
 {
+    private readonly StichtingDbContext _context;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    
+    public BeheerderPortaalController(StichtingDbContext context, SignInManager<IdentityUser> signInManager)
+    {
+        _context = context;
+        _signInManager = signInManager;
+    }
     [HttpGet("GetBedrijven")]
     public async Task<IActionResult> GetBedrijven()
     {
@@ -47,10 +58,41 @@ public class BeheerderPortaalController : ControllerBase
     }
 
 
-    [HttpPost("InviteBedrijf")]
-    public async Task<IActionResult> CreateBedrijf()
+    [HttpGet("InviteBedrijf")]
+    public async Task<IActionResult> InviteBedrijf()
     {
-        // Implementation goes here...
+        Guid identifier = Guid.NewGuid();
+        
+        var beheerder = await _signInManager.UserManager.FindByNameAsync(User.Identity.Name) as Beheerder;
+
+        if (beheerder == null)
+        {
+            return BadRequest("User not found or not a Beheerder");
+        }
+
+        Invite invite = new Invite()
+        {
+            DatumGemaakt = DateTime.Now, 
+            Identifier = identifier, 
+            Uitgever = beheerder, 
+            VerloopDatum = DateTime.Now.AddDays(7)
+        };
+        
+        beheerder.Invites.Add(invite);
+
+
+        await _context.SaveChangesAsync();
+
+        return Ok(identifier.ToString());
+    }
+    
+    [HttpPost("InviteBedrijf")]
+    public async Task<IActionResult> InviteBedrijf(string email)
+    {
+        Guid identifier = Guid.NewGuid();
+        Invite invite = new Invite();
+        
+        
 
         return Ok();
     }
